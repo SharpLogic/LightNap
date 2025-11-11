f7979f10a638dd94215f08d64731416f12e4d7a1
Ed Kaim <ed@sharplogic.com>
2025-11-11 15:13:40 -0800

    Clean up sidebar menu implementation (#44)

--- src/lightnap-ng/src/app/core/features/layout/components/controls/app-sidebar/app-sidebar.component.ts
+++ src/lightnap-ng/src/app/core/features/layout/components/controls/app-sidebar/app-sidebar.component.ts
@@ -1,9 +1,13 @@
-import { Component } from "@angular/core";
-import { AppMenuComponent } from "../app-menu/app-menu.component";
+import { Component, inject } from "@angular/core";
+import { PanelMenuModule } from "primeng/panelmenu";
+import { MenuService } from "@core/features/layout/services/menu.service";
 
 @Component({
   selector: "ln-app-sidebar",
   templateUrl: "./app-sidebar.component.html",
-  imports: [AppMenuComponent],
+  imports: [PanelMenuModule],
 })
-export class AppSidebarComponent {}
+export class AppSidebarComponent {
+  readonly #menuService = inject(MenuService);
+  readonly menuItems = this.#menuService.menuItems;
+}
