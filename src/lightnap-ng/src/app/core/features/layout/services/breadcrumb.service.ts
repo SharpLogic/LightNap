df8903c3013e865fd52b1f382f5c3723038d52ae
Ed Kaim <ed@sharplogic.com>
2025-11-11 18:21:11 -0800

    Cleanup of breadcrumb service

--- src/lightnap-ng/src/app/core/features/layout/services/breadcrumb.service.ts
+++ src/lightnap-ng/src/app/core/features/layout/services/breadcrumb.service.ts
@@ -1,23 +1,18 @@
 import { inject, Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, NavigationEnd, Router } from "@angular/router";
 import { MenuItem } from "primeng/api";
-import { BehaviorSubject, Observable } from "rxjs";
-import { filter } from "rxjs/operators";
+import { filter, map, startWith } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
 })
 export class BreadcrumbService {
   readonly #router = inject(Router);
-  readonly #breadcrumbsSubject = new BehaviorSubject<MenuItem[]>([]);
-  readonly breadcrumbs$: Observable<MenuItem[]> = this.#breadcrumbsSubject.asObservable();
 
-  constructor() {
-    this.#router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
-      const breadcrumbs = this.#createBreadcrumbs(this.#router.routerState.snapshot.root);
-      this.#breadcrumbsSubject.next(breadcrumbs);
-    });
-  }
+  readonly breadcrumbs$ = this.#router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map(() => this.#createBreadcrumbs(this.#router.routerState.snapshot.root))
+  );
 
   #createBreadcrumbs(route: ActivatedRouteSnapshot, url: string = "", breadcrumbs: MenuItem[] = []): MenuItem[] {
     const children: ActivatedRouteSnapshot[] = route.children;
@@ -30,7 +25,8 @@ export class BreadcrumbService {
 
       const breadcrumbData = child.data["breadcrumb"];
       if (breadcrumbData) {
-        // Support both static strings and dynamic functions
+        // Support both static strings and dynamic functions. If you need to do something more complex,
+        // like loading data from an API, consider using a resolver to fetch the data before route activation.
         const label = typeof breadcrumbData === "function" ? breadcrumbData(child) : breadcrumbData;
         const item: MenuItem = { label, routerLink: url };
         breadcrumbs.push(item);
@@ -39,7 +35,9 @@ export class BreadcrumbService {
       return this.#createBreadcrumbs(child, url, breadcrumbs);
     }
 
-    // Remove routerLink from the last breadcrumb (current page)
+    // Remove routerLink from the last breadcrumb (current page). This is kind of a hack because it
+    // assumes the last breadcrumb is always the current page and it looks better without the link.
+    // If you're here wondering why that last breadcrumb isn't rendering as a link, now you know.
     if (breadcrumbs.length > 0) {
       delete breadcrumbs[breadcrumbs.length - 1].routerLink;
     }
