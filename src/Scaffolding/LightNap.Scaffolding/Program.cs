853419be25b9f014fee2644183174833039e93d3
Ed Kaim <ed@sharplogic.com>
2025-11-17 18:18:16 -0800

    Initial scaffolder update to .NET 10

--- src/Scaffolding/LightNap.Scaffolding/Program.cs
+++ src/Scaffolding/LightNap.Scaffolding/Program.cs
@@ -2,40 +2,54 @@
 using LightNap.Scaffolding.ProjectManager;
 using LightNap.Scaffolding.ServiceRunner;
 using System.CommandLine;
-using System.CommandLine.Parsing;
 
 Argument<string> classNameArgument =
-    new("className",
-        description: "The name of the entity to scaffold for");
+    new("className")
+    {
+        Description = "The name of the entity to scaffold for"
+    };
 Option<string> namespaceOption =
-    new("--namespace",
-        getDefaultValue: () => "LightNap.Core.Data.Entities",
-        description: "The namespace of the entity");
+    new("--namespace")
+    {
+        Description = "The namespace of the entity",
+        DefaultValueFactory = _ => "LightNap.Core.Data.Entities",
+    };
 Option<string> srcPathOption =
-    new("--src-path",
-        getDefaultValue: () => "./",
-        description: "The path to the /src folder of the repo");
+    new("--src-path")
+    {
+        Description = "The path to the /src folder of the repo",
+        DefaultValueFactory = _ => "./",
+    };
 Option<string> coreProjectNameOption =
-    new("--core-project",
-        getDefaultValue: () => "LightNap.Core",
-        description: "The name of the core project");
+    new("--core-project")
+    {
+        Description = "The name of the core project",
+        DefaultValueFactory = _ => "LightNap.Core",
+    };
 Option<string> webApiProjectNameOption =
-    new("--web-api-project",
-        getDefaultValue: () => "LightNap.WebApi",
-        description: "The name of the web API project");
+    new("--web-api-project")
+    {
+        Description = "The name of the web API project",
+        DefaultValueFactory = _ => "LightNap.WebApi",
+    };
 Option<string> angularProjectNameOption =
-    new("--angular-project",
-        getDefaultValue: () => "lightnap-ng",
-        description: "The name of the Angular project");
+    new("--angular-project")
+    {
+        Description = "The name of the Angular project",
+        DefaultValueFactory = _ => "lightnap-ng",
+    };
 Option<bool> skipComponentsOption =
-    new("--skip-components",
-        getDefaultValue: () => false,
-        description: "Skip [re]generating Angular components");
+    new("--skip-components")
+    {
+        Description = "Skip [re]generating Angular components",
+        DefaultValueFactory = _ => false,
+    };
 Option<bool> overwriteOption =
-    new("--overwrite",
-        getDefaultValue: () => false,
-        description: "Automatically overwrite files if they exist");
-
+    new("--overwrite")
+    {
+        Description = "Automatically overwrite files if they exist",
+        DefaultValueFactory = _ => false,
+    };
 
 var rootCommand = new RootCommand()
 {
@@ -49,11 +63,14 @@ var rootCommand = new RootCommand()
     overwriteOption
 };
 
-rootCommand.SetHandler((className, namespaceValue, srcPath, coreProjectName, webApiProjectName, angularProjectName, skipComponents, overwrite) =>
-{
-    ServiceRunner runner = new(new ProjectManager(), new AssemblyManager());
-    runner.Run(new ServiceParameters($"{namespaceValue}.{className}", srcPath, namespaceValue, coreProjectName, webApiProjectName, angularProjectName, skipComponents, overwrite));
-},
-classNameArgument, namespaceOption, srcPathOption, coreProjectNameOption, webApiProjectNameOption, angularProjectNameOption, skipComponentsOption, overwriteOption);
+ParseResult parseResult = rootCommand.Parse(args);
 
-return await rootCommand.InvokeAsync(args);
+ServiceRunner runner = new(new ProjectManager(), new AssemblyManager());
+runner.Run(new ServiceParameters($"{parseResult.GetRequiredValue(namespaceOption)}.{parseResult.GetRequiredValue(classNameArgument)}",
+    parseResult.GetRequiredValue(srcPathOption),
+    parseResult.GetRequiredValue(namespaceOption), 
+    parseResult.GetRequiredValue(coreProjectNameOption),
+    parseResult.GetRequiredValue(webApiProjectNameOption), 
+    parseResult.GetRequiredValue(angularProjectNameOption), 
+    parseResult.GetRequiredValue(skipComponentsOption), 
+    parseResult.GetRequiredValue(overwriteOption)));
